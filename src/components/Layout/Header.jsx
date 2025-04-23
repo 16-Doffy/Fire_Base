19dc55e24375ef303cf7e57b58f86daeddfcaf6b
Nhut Duy <ngophamnhutduy050302@gmail.com>
2025-04-23 20:53:15 +0700

    add homepgae header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,114 @@
+import { Button } from "antd";
+import React from "react";
+import styled from "styled-components";
+
+const menuLink = [
+  {
+    url: "/#",
+    title: "Home",
+  },
+  {
+    url: "/#",
+    title: "Blog",
+  },
+  {
+    url: "/#",
+    title: "Contact",
+  },
+];
+const HeaderStyled = styled.header`
+padding: 15px;
+  .header-main {
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+  }
+  .logo {
+    max-width: 100%;
+  }
+  .menu {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    font-size: 22px;
+  }
+  .search {
+    margin-left: auto;
+    padding: 15px 25px;
+    border: 1px solid #eee;
+    border-radius: 8px;
+    width: 100%;
+    max-width: 320px;
+    display: flex;
+    position: relative;
+    align-items: center;
+  }
+  .search-input {
+    flex: 1;
+    padding-right: 45px;
+  }
+  .search-icon {
+    position: absolute;
+    transform: translateY(-5%);
+    right: 15px;
+    
+  }
+  .header-button{
+    margin-left: 20px;
+    width: 200px;
+    height: 50px;
+    background-image: linear-gradient(to right,#6dbfb8, #e0f2fe);
+    font-size: 16px;
+    font-weight: bold;
+    color: #2a2f83;
+  }
+`;
+const Header = () => {
+  return (
+    <HeaderStyled>
+      <div className="container w-full max-w-[1180px] m-auto p-0">
+        <div className="header-main">
+          <a href="/">
+            <img src="/img/mk.png" alt="monkey login" className="logo" />
+          </a>
+          <ul className="menu">
+            {menuLink.map((item) => (
+              <li className="menu-link" key={item.title}>
+                <a href={item.url} className="menu-link">
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className="search">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Search posts...."
+            />
+            <span className="search-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </span>
+          </div>
+          <Button className="header-button">Sign Up</Button>
+        </div>
+      </div>
+    </HeaderStyled>
+  );
+};
+
+export default Header;
--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { auth } from '../FireBase/Firebase-config';
-import { signOut } from 'firebase/auth';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from '../Layout/Header';
 
+
+const HomepageStyle = styled.div`
+
+
+`;
 const Homepage = () => {
-const handleSignOut = () =>{
-    signOut(auth);
-}
+
 
     return (
-        <div className='gap-110'>
-            <button onClick={handleSignOut}>
-            <Link to="/sign-in"> SignOut</Link>
-            <Link to="/sign-up"> Đăng Kí</Link>
-            
-            </button>
-        </div>
+        <HomepageStyle>
+           <Header></Header>
+        </HomepageStyle>
     );
 };
 
